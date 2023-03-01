59496bb968dabf3b4c56904c8b7023b0a3bfd2f6
penghuaping <lansheng228@163.com>
2023-03-01 15:34:13 +0800

    更新升级

--- hello/README.md
+++ hello/README.md
@@ -1,4 +1,5 @@
 ```
 $ javac HelloWorld.java
 $ native-image HelloWorld
+$ ./helloworld
 ```
--- hello/src/HelloWorld.java
+++ hello/src/HelloWorld.java
@@ -1,14 +1,15 @@
 public class HelloWorld {
-
     public static void main(String[] args) {
         String str = "Native Image is awesome";
         String reversed = reverseString(str);
+        System.out.println("The string is: " + str);
         System.out.println("The reversed string is: " + reversed);
     }
 
     public static String reverseString(String str) {
-        if (str.isEmpty())
+        if (str.isEmpty()) {
             return str;
+        }
         return reverseString(str.substring(1)) + str.charAt(0);
     }
 }
